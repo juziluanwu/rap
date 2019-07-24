7eb7ad5b1341739b831981671b5dda963fb6e341
juziluanwu <782265504@qq.com>
2019-07-24 18:12:01 +0800

    rap

--- src/main/java/com/rap/RapApplication.java
+++ src/main/java/com/rap/RapApplication.java
@@ -3,7 +3,9 @@ package com.rap;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @SpringBootApplication
 @MapperScan(basePackages = { "com.rap.mapper.**"})
 public class RapApplication {
---
+++ src/main/java/com/rap/filter/HttpResponseFilter.java
@@ -0,0 +1,40 @@
+package com.rap.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by lowdad on 18-1-30.
+ */
+
+@Component
+public class HttpResponseFilter implements Filter {
+    private final static Logger logger= LoggerFactory.getLogger(HttpResponseFilter.class);
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+    /**
+     * 全局拦截器，做请求头跨域验证
+     */
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response1 = (HttpServletResponse)response;
+        response1.setHeader("Access-Control-Allow-Origin", "*");
+        response1.setHeader("Access-Control-Allow-Methods", "POST,PUT,GET, OPTIONS,DELETE");
+        response1.setHeader("Access-Control-Allow-Headers", "x-requested-with,content-type,token,sign,userid,timestamp");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
