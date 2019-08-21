5f1ad26fc32360de7cf491b0b2c22bbd4671e110
juziluanwu <782265504@qq.com>
2019-08-21 17:24:47 +0800

    打war包

--- src/main/java/com/rap/RapApplication.java
+++ src/main/java/com/rap/RapApplication.java
@@ -3,14 +3,19 @@ package com.rap;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @SpringBootApplication
 @MapperScan(basePackages = { "com.rap.mapper.**"})
-public class RapApplication {
+public class RapApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(RapApplication.class, args);
     }
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(RapApplication.class);
+    }
 }
